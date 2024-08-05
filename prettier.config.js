ae8ef3038a8590fc1b3579c41329d7af4a9caa3f
catrt <rt990122@gmail.com>
2024-08-05 14:19:09 +0900

    Conf: ts eslint prettier config 파일 추가

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 120,
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  useTabs: false,
+  endOfLine: "auto",
+};
+
+export default config;
