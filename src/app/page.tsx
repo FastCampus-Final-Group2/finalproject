46c5d954cf17d40850ad6db92bca3823ea588f5f
catrt <rt990122@gmail.com>
2024-08-20 16:14:06 +0900

    Feat: landing page layout, 로고 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="flex">
+      <div className="flex h-screen w-1/2 items-center justify-center bg-gray-900">
+        <Image src={"/logo.png"} alt="GLT KOREA LOGO" width={436} height={80} />
+      </div>
+      <div className="flex h-screen w-1/2 items-center justify-center bg-blue-30"></div>
+    </div>
+  );
 }
