601519d7ad70492b87b714a34022c2e77b11a0e6
catrt <rt990122@gmail.com>
2024-08-23 15:14:46 +0900

    Fix: SNB 로고 클리시 페이지 이동 기능 제거

--- src/components/SideNavBar/index.tsx
+++ src/components/SideNavBar/index.tsx
@@ -14,13 +14,13 @@ const SideNavBar = () => {
       className={`relative z-nav flex flex-col gap-5 bg-gray-900 pb-[60px] transition-[width] duration-500 ${isSNBOpened ? "w-snb-open" : "w-snb-close"}`}
     >
       <div className={`h-[120px] ${isSNBOpened ? "px-10 py-9" : "px-5 py-[44px]"}`}>
-        <Link href="/dispatch" className="flex items-center justify-center">
+        <div className="flex items-center justify-center">
           {isSNBOpened ? (
             <Image src="/logo_snb_open.png" alt="GLT Korea Logo" width={144} height={48} />
           ) : (
             <Image src="/logo_snb_close.png" alt="GLT Korea Logo" width={32} height={32} />
           )}
-        </Link>
+        </div>
       </div>
       <SideNavBarList />
       <SideNavBarToggleButton />
