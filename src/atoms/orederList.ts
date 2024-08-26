c0713dc4e5813ae352945a2600587958093e2784
catrt <rt990122@gmail.com>
2024-08-26 12:35:51 +0900

    Feat: 업로드 파일 recoil 상태로 저장

--- src/atoms/orederList.ts
+++ src/atoms/orederList.ts
@@ -1,7 +1,7 @@
 import { Order } from "@/types/order";
 import { atom } from "recoil";
 
-export const orderListState = atom<Order[]>({
+export const orderListState = atom<Partial<Order>[]>({
   key: "orderListState",
   default: [],
 });
--- src/components/FileModal/UploadModal/FileInput.tsx/index.tsx
+++ src/components/FileModal/UploadModal/FileInput.tsx/index.tsx
@@ -2,8 +2,10 @@
 
 import { orderListState } from "@/atoms/orederList";
 import Icon from "@/components/core/Icon";
+import { EXCEL_HEADERS } from "@/constants/excel";
+import { Order } from "@/types/order";
 import { useCallback, useEffect, useRef, useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import * as XLSX from "xlsx";
 
 interface FileInputProps {
@@ -13,10 +15,16 @@ interface FileInputProps {
 const FileInput = ({ setIsError }: FileInputProps) => {
   const [excelFile, setExcelFile] = useState<File | null>(null);
   const setOrderList = useSetRecoilState(orderListState);
-
+  const ol = useRecoilValue(orderListState);
+  console.log(ol);
   // TODO
   useEffect(() => {
     if (excelFile) {
+      if (!excelFile.type.includes("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
+        setIsError(true);
+        return;
+      }
+
       const reader = new FileReader();
 
       reader.onload = (event) => {
@@ -25,38 +33,72 @@ const FileInput = ({ setIsError }: FileInputProps) => {
 
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
-        const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-        console.log(data);
-        // setOrderList(data);
+        const excelData = (XLSX.utils.sheet_to_json(worksheet, { header: 1 }) as unknown[][]).filter((row) =>
+          row.some((cell) => cell !== undefined && cell !== ""),
+        );
+        console.log(excelData);
+
+        const isExcelHeaderCorrect = excelData[0].every((header, index) => {
+          return header === EXCEL_HEADERS[index];
+        });
+
+        if (!isExcelHeaderCorrect) {
+          setIsError(true);
+          return;
+        }
+
+        const hasData = excelData.some((row, index) => {
+          if (index < 4) return true;
+          return row.length > 0;
+        });
+
+        if (!hasData) {
+          setIsError(true);
+          return;
+        }
+
+        setOrderList(
+          excelData.slice(4).map((row) => {
+            return {
+              deliveryType: row[0],
+              smName: row[1],
+              shipmentNum: row[2],
+              clientOrderKey: row[3],
+              orderType: row[4],
+              // TODO 날짜 수정
+              receivedDate: new Date(2024, 8, 25),
+              serviceRequestDate: new Date(2024, 9, 1),
+              serviceRequestTime: new Date(2024, 9, 1, 15, 30),
+              clientName: row[8],
+              contact: row[9],
+              address: row[10],
+              detailAddress: row[11],
+              zipcode: row[12],
+              volume: Number(row[13]),
+              weight: Number(row[14]),
+              note: row[15],
+              expectedServiceDuration: row[16] ? Number(row[16]) : undefined,
+              productName: row[17],
+              productCode: row[18],
+              productQuantity: Number(row[19]),
+            } as Partial<Order>;
+          }),
+        );
       };
 
       reader.readAsArrayBuffer(excelFile);
     }
-  }, [excelFile, setIsError]);
-
-  const validateAndSetFile = (uploadedFile: File | null) => {
-    if (!uploadedFile) {
-      setIsError(true);
-      return;
-    }
-
-    if (!uploadedFile.type.includes("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
-      setIsError(true);
-      return;
-    }
-
-    setExcelFile(uploadedFile);
-  };
+  }, [excelFile, setIsError, setOrderList]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const uploadedFile = e.target.files?.[0] || null;
-    validateAndSetFile(uploadedFile);
+    setExcelFile(uploadedFile);
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     const uploadedFile = e.dataTransfer.files?.[0] || null;
-    validateAndSetFile(uploadedFile);
+    setExcelFile(uploadedFile);
   };
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
