bc90dcb9212bd7cbc55f9d314bb49089bbda685d
ldh9669 <leedonghee960@gmail.com>
2024-09-01 16:45:14 +0900

    Feat: 지도 컴포넌트 ui 추가

--- src/app/(withAuth)/dispatch/manual/page.tsx
+++ src/app/(withAuth)/dispatch/manual/page.tsx
@@ -1,12 +1,18 @@
-import DispatchInformationHeader from '@/components/DispatchInformationHeader';
-import OrderDashBoard from '@/components/OrderDashBoard';
+import DispatchInformationHeader from "@/components/DispatchInformationHeader";
+import OrderDashBoard from "@/components/OrderDashBoard";
+import NaverMap from "@/components/core/NaverMap";
 
 const page = () => {
-  return(
-  <>
-    <DispatchInformationHeader />
-    <OrderDashBoard />
-  </>
+  return (
+    <>
+      <DispatchInformationHeader />
+      <div className="flex w-full">
+        <OrderDashBoard />
+        <div className="w-full">
+          <NaverMap latitude={37.5665} longitude={126.978} />
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -50,110 +50,70 @@ const orange = {
   600: "#BC4410",
 };
 
-<<<<<<< HEAD
 const lime = {
-=======
-const lime ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#EEFAEA",
   100: "#DEF5D6",
   350: "#B0DB70",
   650: "#3F9122",
 };
 
-<<<<<<< HEAD
 const sky = {
-=======
-const sky ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#EAF7FA",
   100: "#D6EFF5",
   350: "#70C8DC",
   650: "#237B8F",
 };
 
-<<<<<<< HEAD
 const purple = {
-=======
-const purple ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#EFEAFA",
   100: "#DED6F5",
   350: "#8C70DC",
   650: "#3F238F",
 };
 
-<<<<<<< HEAD
 const violet = {
-=======
-const violet ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#FAEAF7",
   100: "#F5D6EE",
   350: "#DC70C4",
   650: "#8F2377",
 };
 
-<<<<<<< HEAD
 const redwood = {
-=======
-const redwood ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#FAEDEA",
   100: "#F5DCD6",
   350: "#DC8370",
   650: "#8F3623",
 };
 
-<<<<<<< HEAD
 const peanut = {
-=======
-const peanut ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#FAF5EA",
   100: "#F5EAD6",
   350: "#DCB770",
   650: "#8F6A23",
 };
 
-<<<<<<< HEAD
 const brown = {
-=======
-const brown ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#F8F1ED",
   100: "#F1E4DA",
   350: "#CD9F7E",
   650: "#805332",
 };
 
-<<<<<<< HEAD
 const forest = {
-=======
-const forest ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#EFF6EF",
   100: "#DFECE0",
   350: "#8DBE91",
   650: "#417245",
 };
 
-<<<<<<< HEAD
 const yale = {
-=======
-const yale ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#ECF2F8",
   100: "#DAE4F2",
   350: "#7CA2D0",
   650: "#2F5683",
 };
 
-<<<<<<< HEAD
 const olive = {
-=======
-const olive ={
->>>>>>> cbc6cfb (Feat: 배차관리 dashboard 추가)
   50: "#F6F8ED",
   100: "#EEF1DA",
   350: "#C3CE7E",
