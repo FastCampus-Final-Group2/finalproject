acbfc025780acc9f6884eb875acc36c9f3524f2c
catrt <rt990122@gmail.com>
2024-09-07 15:26:59 +0900

    Feat: 업로드 취소시 confirm modal 연결

--- src/components/OrderValidation/OrderValidationTable/TabList/Buttons/index.tsx
+++ src/components/OrderValidation/OrderValidationTable/TabList/Buttons/index.tsx
@@ -2,12 +2,16 @@
 
 import { TransportAPI } from "@/apis/transportOrder";
 import { dispatchNameState, excelDataState, isValidExcelDataState, loadingStartTimeState } from "@/atoms/excelData";
+import ConfirmModal from "@/components/ConfirmModal";
 import Button from "@/components/core/Button";
 import useResetExcelDataAtoms from "@/hooks/useResetExcelDataAtoms";
 import { formatTransportOrderRequest } from "@/utils/format/transportOrder";
+import { useState } from "react";
 import { useRecoilValue } from "recoil";
 
 const Buttons = () => {
+  const [cancelModalOpen, setCancelModalOpen] = useState(false);
+
   const resetExcelDataAtoms = useResetExcelDataAtoms();
 
   const loadingStartTime = useRecoilValue(loadingStartTimeState);
@@ -16,7 +20,7 @@ const Buttons = () => {
   const isValidExcelData = useRecoilValue(isValidExcelDataState);
 
   const handleClickCancelBtn = () => {
-    resetExcelDataAtoms();
+    setCancelModalOpen(true);
   };
 
   const handleClickDispathBtn = async () => {
@@ -41,6 +45,20 @@ const Buttons = () => {
       >
         배차 진행
       </Button>
+      {cancelModalOpen && (
+        <ConfirmModal
+          title="주문업로드를 취소하시겠습니까?"
+          text={[{ type: "alert", value: "취소 시, 업로드 된 주문 목록이 사라집니다." }]}
+          onClickClose={() => {
+            setCancelModalOpen(false);
+          }}
+          onConfirm={() => {
+            resetExcelDataAtoms();
+          }}
+          leftButtonText="돌아가기"
+          rightButtonText="업로드 취소"
+        />
+      )}
     </div>
   );
 };
