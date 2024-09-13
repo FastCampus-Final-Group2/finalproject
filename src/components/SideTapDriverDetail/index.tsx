796de9138883c7b809de88fbc82affb612ad732d
ldh9669 <leedonghee960@gmail.com>
2024-09-13 16:39:38 +0900

    Feat: 배차 확정

--- src/components/SideTapDriverDetail/index.tsx
+++ src/components/SideTapDriverDetail/index.tsx
@@ -6,14 +6,44 @@ import Icon from "@/components/core/Icon";
 import { TEXT_650, BG_50 } from "@/styles/smColor";
 import { useRecoilValue } from "recoil";
 import { bgColorState } from "@/atoms/bgColorState";
+import { transportOrderState } from "@/atoms/transportOrder";
+import { driverIndex } from "@/atoms/driverIndex";
 
 export interface ListStopOverData {
-  id: number;
-  warningCheck: boolean;
-  errorMessage: string;
-  address: string;
-  meter: number;
-  kilogram: number;
+  restrictedTonCode: boolean;
+  delayRequestTime: boolean;
+  overContractNum: boolean;
+  ett: number;
+  expectationOperationStartTime: string;
+  expectationOperationEndTime: string;
+  deliveryDestinationId: number;
+  managerName: string | null;
+  phoneNumber: string | null;
+  lat: number;
+  lon: number;
+  distance: number;
+  deliveryType: string;
+  smId: number;
+  smName: string;
+  shipmentNumber: string;
+  clientOrderKey: string;
+  orderType: string;
+  receivedDate: string;
+  serviceRequestDate: string;
+  serviceRequestTime: string;
+  clientName: string;
+  contact: string;
+  roadAddress: string;
+  lotNumberAddress: string;
+  detailAddress: string;
+  zipcode: string;
+  volume: number;
+  weight: number;
+  note: string;
+  expectedServiceDuration: number;
+  productName: string;
+  productCode: string;
+  productQuantity: number;
 }
 
 interface SideTapDriverDetailProps {
@@ -32,16 +62,23 @@ const SideTapDriverDetail = ({
   toggleExpand,
 }: SideTapDriverDetailProps) => {
   const bgColor = useRecoilValue(bgColorState);
+  const driverIndexState = useRecoilValue(driverIndex);
+  const dispatchData = useRecoilValue(transportOrderState);
 
   return (
     <div>
       {isSideTapExpanded && (
         <div className="relative h-[884px] w-[440px]">
           <DriverDispatchDetailDashboard
-            drivingTime={16}
-            mileage={23}
-            totalOrder={81}
+            drivingTime={dispatchData.course[driverIndexState].totalTime}
+            mileage={dispatchData.course[driverIndexState].mileage}
+            totalOrder={dispatchData.course[driverIndexState].orderNum}
             availabilityOrder={80}
+            floorAreaRatio={dispatchData.course[driverIndexState].floorAreaRatio}
+            driverName={dispatchData.course[driverIndexState].smName}
+            driverPhoneNumber={dispatchData.course[driverIndexState].smPhoneNumber}
+            vehicleType={dispatchData.course[driverIndexState].vehicleType}
+            vehicleTon={dispatchData.course[driverIndexState].vehicleTon}
             bgColor={bgColor}
           />
           <StopOverList
